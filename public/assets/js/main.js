bcaf1dffd055453808c78337dca88898e4bbabaf
Ilia Abedian <eilia.abedian@yahoo.com>
2022-04-27 15:40:22 -0700

    initial naming files and folder
    - moved the common css to the _partial ejs
    - moved the common js to the _partial ejs
    - renaming the entrypoint frontend js file.

---
+++ public/assets/js/main.js
@@ -0,0 +1,22 @@
+document.querySelector('body').addEventListener('click', async event => {
+    event.preventDefault();
+});
+
+document.querySelector('form.gameMoveForm button').addEventListener('click', async event => {
+    const target = event.target;
+    const theForm = target.closest('form');
+    const moveValue = theForm.querySelector('#move').value;
+
+    const responseFromApi = await axios.post('/api/games/submitMove',
+        { move: moveValue }
+    );
+
+    const resultDiv = document.querySelector('div#resultOfMatch');
+
+    const result = responseFromApi.data;
+    if (result.status) {
+        resultDiv.innerHTML = `<h2>You ${result.result}!</h2>`;
+    } else {
+        console.log(result);
+    }
+});
