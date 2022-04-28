1ebe4a089331a33534e362bccf2c526d1b978177
Yevgeniy Akimenko <yakimenko@bcit.ca>
2022-04-28 16:35:17 -0700

    finished the basic round processing, without lives or inventory counters

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -6,13 +6,126 @@ let currentSelectedInventory;
 
 const gameResultEnum = ['loss', 'win', 'draw'];
 
+function singleCompare(move1, move2) {
+    if (move1 === 'attack' && move2 === null) {
+      return 1;
+    } else if (move1 === null && move2 === 'attack') {
+      return 2;
+    } else {
+      return 0;
+    }
+  };
+
+function tripletCompare(moves) {
+    let ones = moves.filter(number => number === 1).length;
+    let twos = moves.filter(number => number === 2).length;
+
+    if (ones > twos) {
+        return 1
+    } else if (ones < twos) {
+        return 2
+    } else {
+        return 0;
+    }
+}
+
+  const acceptableMovesEnums = ["attack", "block", "none"];
+
+  const acceptableMoves = {
+      "attack": "a",
+      "block": "b",
+      "none": ""
+  };
+
+  const results = {
+      "player 1 wins": "won",
+      "player 2 wins": "lost",
+      "nothing happened": "draw"
+  };
+
+  const getRandomMove = () => acceptableMovesEnums[Math.floor(Math.random() * acceptableMovesEnums.length)];
+
+  
+
+  /* const opponentMove = {
+      'head': getRandomMove(),
+      'body': getRandomMove(),
+      'legs': getRandomMove()
+  } */
+
+  const opponentMove = {
+    'head': 'attack',
+    'body': 'block',
+    'legs': null
+    }
+
+  console.log('opponentMove', opponentMove);
 
 document.querySelector('body').addEventListener('click', async event => {
     event.preventDefault();
 });
 
-document.querySelector('button.done').addEventListener('click', async event => {
+document.querySelector('div.done').addEventListener('click', async event => {
     console.log("HERE")
+    let myTallyFirstColumn = document.querySelectorAll('table.tally.my-tally td:first-child');
+   
+    console.log('my', myTallyFirstColumn);
+    
+    myTallyFirstColumn.forEach((td, index) => {
+        let moveComponent = Object.values(Move.myMoves)[index];
+        if (moveComponent === 'attack') {
+            td.classList.add('cell-attacked');
+        } else if (moveComponent === 'block') {
+            td.classList.add('cell-blocked');
+        }
+        
+    });
+
+    let opponentTallyFirstColumn = document.querySelectorAll('table.tally.opponent-tally td:first-child');
+    console.log('opponent', opponentTallyFirstColumn);
+    opponentTallyFirstColumn.forEach((td, index) => {
+        let moveComponent = Object.values(opponentMove)[index];
+        if (moveComponent === 'attack') {
+            td.classList.add('cell-attacked');
+        } else if (moveComponent === 'block') {
+            td.classList.add('cell-blocked');
+        }
+        
+    });
+
+    let playerMoves = [];
+    for (let index = 0; index < 3; index++) {
+        let myMoveComponent = Object.values(Move.myMoves)[index];
+        let opponentMoveComponent = Object.values(opponentMove)[index];
+        playerMoves.push(singleCompare(myMoveComponent, opponentMoveComponent));
+    }
+    
+    console.log('playermoves', playerMoves);
+
+    let roundResult = tripletCompare(playerMoves);
+    if (roundResult === 1) {
+        myTallyFirstColumn.forEach(td => {
+            td.classList.add('round-won');
+        });
+        opponentTallyFirstColumn.forEach(td => {
+            td.classList.add('round-defeat');
+        });
+    } else if (roundResult === 2) {
+        myTallyFirstColumn.forEach(td => {
+            td.classList.add('round-defeat');
+        });
+        opponentTallyFirstColumn.forEach(td => {
+            td.classList.add('round-won');
+        });
+    } else {
+        myTallyFirstColumn.forEach(td => {
+            td.classList.add('round-draw');
+        });
+        opponentTallyFirstColumn.forEach(td => {
+            td.classList.add('round-draw');
+        });
+    }
+    
 });
 
 document.querySelector('img.my-shield').addEventListener('click', async event => {
