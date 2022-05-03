471f315f8c9fdbcabbde51aee4b34d69af0aba3a
Ilia Abedianamiri <37903573+iliaamiri@users.noreply.github.com>
2022-05-02 18:16:53 -0700

    un-hard code computer moves

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,85 +1,65 @@
-import Move from './classes/Move.js';
 import MovePlaceholder from './classes/MovePlaceholder.js';
 import Inventory from "./classes/Inventory.js";
 import Life from './classes/Life.js';
+
+// Helpers
+import Players from "./helpers/Players.js";
+import Player from "./helpers/Player.js";
+import BotPlayer from "./helpers/BotPlayer.js";
+import RoundMove from "./helpers/RoundMove.js";
+
+// Initiating the players.
+Players.all.player1 = new Player("AklBm4", "Me");
+Players.all.player2 = new BotPlayer();
 
 let currentSelectedInventory;
 
 const gameResultEnum = ['loss', 'win', 'draw'];
 
 function singleCompare(move1, move2) {
-    if (move1 === 'attack' && move2 === null) {
-      return 1;
-    } else if (move1 === null && move2 === 'attack') {
-      return 2;
+    if (move1 === 'attack' && move2 === 'none') {
+        return 1;
+    } else if (move1 === 'none' && move2 === 'attack') {
+        return 2;
     } else {
-      return 0;
+        return 0;
     }
-  };
+}
 
 function tripletCompare(moves) {
     let ones = moves.filter(number => number === 1).length;
     let twos = moves.filter(number => number === 2).length;
 
     if (ones > twos) {
-        return 1
+        return 1;
     } else if (ones < twos) {
-        return 2
+        return 2;
     } else {
         return 0;
     }
 }
-
-  const acceptableMovesEnums = ["attack", "block", "none"];
-
-  const acceptableMoves = {
-      "attack": "a",
-      "block": "b",
-      "none": ""
-  };
-
-  const results = {
-      "player 1 wins": "won",
-      "player 2 wins": "lost",
-      "nothing happened": "draw"
-  };
-
-  const getRandomMove = () => acceptableMovesEnums[Math.floor(Math.random() * acceptableMovesEnums.length)];
-
-  
-
-  /* const opponentMove = {
-      'head': getRandomMove(),
-      'body': getRandomMove(),
-      'legs': getRandomMove()
-  } */
-
-  const opponentMove = {
-    'head': 'attack',
-    'body': 'block',
-    'legs': null
-    }
-
-  console.log('opponentMove', opponentMove);
 
 document.querySelector('body').addEventListener('click', async event => {
     event.preventDefault();
 });
 
 document.querySelector('div.done').addEventListener('click', async event => {
+    // Generating the Bot player's moves.
+    const opponentMove = Players.all.player2.generateRandomMoves();
+
     console.log("HERE")
     let myTallyFirstColumn = document.querySelectorAll('table.tally.my-tally td:first-child');
-   
+
     console.log('my', myTallyFirstColumn);
-    
+
     myTallyFirstColumn.forEach((td, index) => {
-        let moveComponent = Object.values(Move.myMoves)[index];
+        let moveComponent = Object.values(Players.all.player1.moves.toJSON())[index];
         if (moveComponent === 'attack') {
             td.classList.add('cell-attacked');
         } else if (moveComponent === 'block') {
             td.classList.add('cell-blocked');
         }
-        
+
     });
 
     let opponentTallyFirstColumn = document.querySelectorAll('table.tally.opponent-tally td:first-child');
@@ -88,21 +68,19 @@ document.querySelector('div.done').addEventListener('click', async event => {
         let moveComponent = Object.values(opponentMove)[index];
         if (moveComponent === 'attack') {
             td.classList.add('cell-attacked');
-            Inventory.all.opponentAttack.decreaseCounter();
         } else if (moveComponent === 'block') {
             td.classList.add('cell-blocked');
-            Inventory.all.opponentBlock.decreaseCounter();
         }
-        
+
     });
 
     let playerMoves = [];
     for (let index = 0; index < 3; index++) {
-        let myMoveComponent = Object.values(Move.myMoves)[index];
+        let myMoveComponent = Object.values(Players.all.player1.moves.toJSON())[index];
         let opponentMoveComponent = Object.values(opponentMove)[index];
         playerMoves.push(singleCompare(myMoveComponent, opponentMoveComponent));
     }
-    
+
     console.log('playermoves', playerMoves);
 
     let roundResult = tripletCompare(playerMoves);
@@ -130,9 +108,9 @@ document.querySelector('div.done').addEventListener('click', async event => {
             td.classList.add('round-draw');
         });
     }
-    
+
     document.querySelectorAll('.show-animation').forEach(element => {
-        console.log('element',element);
+        console.log('element', element);
         element.classList.add('hide-animation');
         element.classList.remove('show-animation');
     });
@@ -144,24 +122,24 @@ document.querySelector('div.done').addEventListener('click', async event => {
 
 document.querySelector('img.my-shield').addEventListener('click', async event => {
     console.log('shield success!');
-    Move.selectedMoveType = 'block';
+    RoundMove.selectedMoveType = 'block';
     const myBlockCounter = document.querySelector('span.my-block-counter');
     currentSelectedInventory = Inventory.all['myBlock'];
 });
 
 document.querySelector('img.my-attack').addEventListener('click', async event => {
     console.log('sword success!');
-    Move.selectedMoveType = 'attack';
+    RoundMove.selectedMoveType = 'attack';
     const myAttackCounter = document.querySelector('span.my-attack-counter');
     currentSelectedInventory = Inventory.all['myAttack'];
 });
 
 document.querySelector('div.moves-placeholder').addEventListener('click', async event => {
     let target = event.target;
-    console.log('selectedMoveType', Move.selectedMoveType);
-    if (target.tagName === 'DIV' && Move.selectedMoveType && target.classList.contains('mv-placeholder')) {
+    console.log('selectedMoveType', RoundMove.selectedMoveType);
+    if (target.tagName === 'DIV' && RoundMove.selectedMoveType && target.classList.contains('mv-placeholder')) {
         let bodyPartType;
-        if (target.classList.contains('head') ) {
+        if (target.classList.contains('head')) {
             console.log('hitting head');
             bodyPartType = 'head';
 
@@ -172,22 +150,21 @@ document.querySelector('div.moves-placeholder').addEventListener('click', async 
         } else if (target.classList.contains('legs')) {
             console.log('hitting legs');
             bodyPartType = 'legs';
-
         }
 
         let currentMovePlaceholder = MovePlaceholder.all[bodyPartType];
         if (currentMovePlaceholder) {
             currentMovePlaceholder.bodyPartType = bodyPartType;
-            currentMovePlaceholder.moveType = Move.selectedMoveType;
+            currentMovePlaceholder.moveType = RoundMove.selectedMoveType;
             currentMovePlaceholder.target = target;
         } else {
-            MovePlaceholder.all[bodyPartType] = new MovePlaceholder(bodyPartType, Move.selectedMoveType, target);
+            MovePlaceholder.all[bodyPartType] = new MovePlaceholder(bodyPartType, RoundMove.selectedMoveType, target);
             currentMovePlaceholder = MovePlaceholder.all[bodyPartType];
         }
 
         currentMovePlaceholder.check();
         console.log(currentMovePlaceholder);
-        console.log('my moves object', Move.myMoves);
+        console.log('my moves object', Players.all.player1.moves);
     }
 });
 
