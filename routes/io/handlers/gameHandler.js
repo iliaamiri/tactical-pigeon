3cb77a275e9d75e69f4c8eff603b738a0fea2681
Ilia Abedian <eilia.abedian@yahoo.com>
2022-05-09 13:33:27 -0700

    move handlers app/handlers -> routes/io/handlers

---
+++ routes/io/handlers/gameHandler.js
@@ -0,0 +1,12 @@
+module.exports = (io, socket) => {
+    const searchForOpponent = () => {
+        
+    }
+
+    const end = () => {
+
+    }
+
+    soclet.on("game:searchForOpponent", searchForOpponent)
+    socket.on("game:end", end);
+}
