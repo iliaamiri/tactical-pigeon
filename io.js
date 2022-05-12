fbfced772ba20486613f654995f29e2bb6513761
Ilia Abedian <eilia.abedian@yahoo.com>
2022-05-12 12:50:11 -0700

    reformat indentation root files

--- io.js
+++ io.js
@@ -3,17 +3,17 @@ const userAuthMiddleware = require("./app/io/middlewares/userAuth");
 const gameHandler = require("./app/io/handlers/gameHandler");
 
 module.exports = (server) => {
-    const io = require('socket.io')(server);
+  const io = require('socket.io')(server);
 
-    io.use(userAuthMiddleware);
-    
-    io.on('connection', async (socket) => {
-        try {
-            await gameHandler(io, socket);
-        } catch (err) {
-            console.log(err);
-        }
-    });
+  io.use(userAuthMiddleware);
 
-    return io;
+  io.on('connection', async (socket) => {
+    try {
+      await gameHandler(io, socket);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  return io;
 }
