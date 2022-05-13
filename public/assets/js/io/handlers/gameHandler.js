1bbaccaf10a4a623d565d274c57eb37a9f02e85f
Ilia Abedianamiri <37903573+iliaamiri@users.noreply.github.com>
2022-05-12 20:46:51 -0700

    🏌️redirect the user to /play/:gameId once the match was found.

--- public/assets/js/io/handlers/gameHandler.js
+++ public/assets/js/io/handlers/gameHandler.js
@@ -1,7 +1,12 @@
-export default (io, socket) => {
+export default async (io, socket) => {
   const matchFound = (payload) => {
-    const {gameId, player2_username} = payload;
+    console.log("match found. payload: ", payload);
+    const { gameId, players } = payload;
+
+    // let game = new Game()
+    // game.save()
+    location.href = "/play/" + gameId;
   }
 
-  socket.on('game:matchFound', matchFound)
+  socket.on('game:matchFound', matchFound);
 };
