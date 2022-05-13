41d1889778cc76aa0d9f113bffbf0b5385872089
Ilia Abedianamiri <37903573+iliaamiri@users.noreply.github.com>
2022-05-12 20:44:30 -0700

    ⚒️ use Token.verifyIntegrity method to validate the JWT token.

--- app/io/middlewares/userAuth.js
+++ app/io/middlewares/userAuth.js
@@ -1,6 +1,3 @@
-const jwt = require("jsonwebtoken");
-const configs = include("config/app.config");
-
 const authExceptions = include('core/Exceptions/AuthExceptions');
 
 const Tokens = include("app/repos/Tokens");
@@ -28,7 +25,7 @@ module.exports = (socket, next) => {
   if (!foundPlayer) {
     foundPlayer = Object.create(Player);
     try {
-      let decodedData = jwt.verify(jwtToken, configs.JWT_RSA_PUBLIC_KEY, {algorithm: "RS256"});
+      let decodedData = Tokens.verifyIntegrity(jwtToken);
       foundPlayer.initOnlinePlayer(decodedData.playerId, decodedData.username);
       Players.add(foundPlayer);
     } catch (err) {
