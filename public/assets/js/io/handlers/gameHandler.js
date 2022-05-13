f76f5e9abc4fec01ab65f5c22c018ffb6e986536
Ilia Abedianamiri <37903573+iliaamiri@users.noreply.github.com>
2022-05-12 21:07:35 -0700

    ➕🎨add UI exhibits and redirects + comments

--- public/assets/js/io/handlers/gameHandler.js
+++ public/assets/js/io/handlers/gameHandler.js
@@ -1,11 +1,27 @@
+// Components
+import {searchingText, searchingForOpponentAnimation} from "../../routes/home.js";
+
+// Core
+import {playSound, sounds} from "../../core/sounds.js";
+
 export default async (io, socket) => {
   const matchFound = (payload) => {
     console.log("match found. payload: ", payload);
+
     const { gameId, players } = payload;
 
+    // TODO: Initiate a new game and save gameId and players to localStorage. (CACHE)
     // let game = new Game()
     // game.save()
-    location.href = "/play/" + gameId;
+
+    // UI exhibits
+    playSound(sounds.winRound);
+    clearInterval(searchingForOpponentAnimation);
+    searchingText.innerHTML = "Match found!";
+
+    setTimeout(() => {
+      location.href = "/play/" + gameId;
+    }, 3000);
   }
 
   socket.on('game:matchFound', matchFound);
