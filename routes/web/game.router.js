72dffc1ef7ae343ac2dd57eea50b9709eb92722c
Ilia Abedianamiri <37903573+iliaamiri@users.noreply.github.com>
2022-05-14 00:10:09 -0700

    📃add comments for /play/:gameId route flow

--- routes/web/game.router.js
+++ routes/web/game.router.js
@@ -2,6 +2,8 @@ const express = require('express');
 const GameController = require('../../app/http/controllers/gameController');
 const gameRouter = express.Router();
 
+// Everything here starts from: /play
 gameRouter.get('/', GameController.showGamePage);
+gameRouter.get('/:gameId', GameController.showOnlinePlay)
 
 module.exports = gameRouter;
