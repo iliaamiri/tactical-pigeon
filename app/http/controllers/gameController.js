a1f74c3b32e66c1e838997bb21e5b13962547af3
Yevgeniy Akimenko <yakimenko@bcit.ca>
2022-05-16 13:56:34 -0700

    some progress???

--- app/http/controllers/gameController.js
+++ app/http/controllers/gameController.js
@@ -1,11 +1,16 @@
 const singleCompare = include("./test/compare-moves");
+const Games = include('./app/repos/Games');
+const Tokens = include('./app/repos/Tokens');
+const Players = include("app/repos/Players").Players;
+
+const Player = include("app/models/Player");
 
 const GameController = {
   async submitGameMove(req, res) {
     try {
-      console.log("api /games/submitMove post hit");
-      const {move} = req.body;
-      console.log("Move: ", move);
+      // console.log("api /games/submitMove post hit");
+      const { move } = req.body;
+      // console.log("Move: ", move);
       const acceptableMovesEnums = ["attack", "block", "none"];
       const acceptableMoves = {
         "attack": "a",
@@ -43,22 +48,72 @@ const GameController = {
   },
 
   async showOnlinePlay(req, res) {
-    // req.body.gameId
+    console.log('showOnlinePlay req.params:', req.params);
+    const gameId = req.params.gameId;
 
     // we need to make sure that the gameId exists in the Games.all
-    // Games.find(gameId);
+    const game = Games.find(gameId);
 
-    // the user should be authenticated at this point.
+    // the user should be authenticated at this point
+    console.log('showOnlinePlay req.session.JWT:', req.cookies.JWT);
+    const jwtToken = req.cookies.JWT;
 
-    // we need to check if the authenticated user is one the players of the game.
+    if (!jwtToken) {
+      next(authExceptions.authFailed.errMessage);
+      return;
+    }
+  
+    const foundTokenObj = Tokens.all.get(jwtToken);
+  
+    if (!foundTokenObj) {
+      next(authExceptions.authFailed.errMessage);
+      return;
+    }
+  
+    let foundPlayer = Players.find(foundTokenObj.playerId);
+    console.log('foundPlyaerId:', foundPlayer.playerId);
+  
+    /* if (!foundPlayer) {
+      foundPlayer = Object.create(Player);
+      try {
+        let decodedData = Tokens.verifyIntegrity(jwtToken);
+        foundPlayer.initOnlinePlayer(decodedData.playerId, decodedData.username);
+        Players.add(foundPlayer);
+      } catch (err) {
+        next(err);
+        return;
+      }
+    } */
 
-    // check if the game is still ongoing and not finished.
+    let myUsername;
+    let opponentUsername;
+
+    // we need to check if the authenticated user is one the players of the game
+    if (!game.players.includes(foundPlayer.playerId)) {
+      // throw an error
+      res.render('layouts/404');
+      return;
+    }
+
+    let newArr = JSON.parse(JSON.stringify(game.players));
+    const index = newArr.indexOf(foundPlayer.playerId);
+    if (index > -1) {
+      newArr.splice(index, 1); // 2nd parameter means remove one item only
+    }
+    myUsername = foundPlayer.playerId;
+    opponentUsername = newArr[0];
+    console.log('myId:', myUsername);
+    console.log('opponentId:', opponentUsername);
+
+    // check if the game is still ongoing and not finished
+    let gameComplete = (game.gameComplete === true) ? true : false;
 
     res.render('play', {
       playMode: "online",
-      gameId: "<gameId>",
-      myUsername: "<myUsername>",
-      opponentUsername: "<opponentUsername>"
+      gameId: gameId,
+      myUsername: myUsername,
+      gameComplete: gameComplete,
+      opponentUsername: opponentUsername
     });
   }
 };
