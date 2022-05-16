4472aedcf8f4cb810b1085f53c26ed929d7a4c6e
Ilia Abedianamiri <37903573+iliaamiri@users.noreply.github.com>
2022-05-16 16:53:38 -0700

    ➕📃add userErrorMessage to the exceptions.

--- core/Exceptions/AuthExceptions.js
+++ core/Exceptions/AuthExceptions.js
@@ -1,14 +1,17 @@
 const AuthExceptions = {
   badInput: {
     errMessage: new Error("INVALID_CREDENTIALS"),
+    userErrorMessage: "Invalid input. Please try again.",
     httpStatus: 400
   },
   authFailed: {
     errMessage: new Error("AUTHENTICATION_FAILED"),
+    userErrorMessage: "Failed to login. Please try again.",
     httpStatus: 403
   },
   alreadyInMatch: {
     errMessage: new Error("ALREADY_IN_GAME"),
+    userErrorMessage: "User is already in match.",
     httpStatus: 400
   }
 };
--- core/Exceptions/GameExceptions.js
+++ core/Exceptions/GameExceptions.js
@@ -1,10 +1,12 @@
 const GameExceptions = {
   currentlyInGame: {
     errMessage: new Error("PLAYER_IS_ALREADY_IN_GAME"),
+    userErrorMessage: "Player is already in the game.",
     httpStatus: 400
   },
   gameNotFound: {
     errMessage: new Error("GAME_NOT_FOUND"),
+    userErrorMessage: "Game does not exist",
     httpStatus: 404
   }
 };
