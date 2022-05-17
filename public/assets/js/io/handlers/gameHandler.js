8d4cb73faa1bd8c5c484d6ccc0a041940d12f055
Ilia Abedianamiri <37903573+iliaamiri@users.noreply.github.com>
2022-05-17 00:38:51 -0700

    💪handle the `opponentMove` socket handler in frontend

--- public/assets/js/io/handlers/gameHandler.js
+++ public/assets/js/io/handlers/gameHandler.js
@@ -33,8 +33,17 @@ export default async (io, socket) => {
     );
   };
 
-  const receiveOpponentMoves = (moves, gameComplete) => {
+  const receiveOpponentMoves = (payload) => {
+    const { opponentMoves, gameComplete } = payload;
+    console.log("Opponent Moves received: ", opponentMoves);
+    console.log("Game Complete: ", gameComplete);
 
+    document.dispatchEvent(
+      new CustomEvent('opponentMoveReady', {
+        detail: {
+          opponentMoves, gameComplete
+        }
+    )
   };
 
   socket.on('game:matchFound', matchFound);
