61e246fb60bcec507b68ce2b5a78f865fdc2de08
Ilia Abedianamiri <37903573+iliaamiri@users.noreply.github.com>
2022-05-18 19:09:01 -0700

    add the apiUserAuth middleware to /api/auth/letMeIn

--- routes/api/auth.router.js
+++ routes/api/auth.router.js
@@ -3,8 +3,11 @@ const authRouter = express.Router();
 
 const AuthController = require('../../app/http/controllers/authController');
 
+// Middlewares
+const apiUserAuth = require('../../app/http/middlewares/apiUserAuth');
+
 // Everything starts from /api/auth/
-authRouter.post("/letMeIn", AuthController.loginOrSignUp);
+authRouter.post("/letMeIn", apiUserAuth(false), AuthController.loginOrSignUp);
 
 
 module.exports = authRouter;
