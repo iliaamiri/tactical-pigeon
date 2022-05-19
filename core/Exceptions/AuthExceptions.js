1339b918b0511f624d29262ff7c45da0acb13395
Ilia Abedianamiri <37903573+iliaamiri@users.noreply.github.com>
2022-05-18 19:08:35 -0700

    revise the AuthExceptions objects

--- core/Exceptions/AuthExceptions.js
+++ core/Exceptions/AuthExceptions.js
@@ -1,18 +1,23 @@
 const AuthExceptions = {
   badInput: {
-    errMessage: new Error("INVALID_CREDENTIALS"),
+    errMessage: "INVALID_CREDENTIALS",
     userErrorMessage: "Invalid input. Please try again.",
     httpStatus: 400
   },
   authFailed: {
-    errMessage: new Error("AUTHENTICATION_FAILED"),
+    errMessage: "AUTHENTICATION_FAILED",
     userErrorMessage: "Failed to login. Please try again.",
     httpStatus: 403
   },
   alreadyInMatch: {
-    errMessage: new Error("ALREADY_IN_GAME"),
+    errMessage: "ALREADY_IN_GAME",
     userErrorMessage: "User is already in match.",
     httpStatus: 400
+  },
+  alreadyAuthenticated: {
+    errMessage: "ALREADY_AUTHENTICATED",
+    userErrorMessage: "You are already logged-in.",
+    httpStatus: 200
   }
 };
 module.exports = AuthExceptions;
