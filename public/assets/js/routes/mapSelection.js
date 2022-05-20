ae7d9bfb9701080c8cf56cd5bb4bf4d9e7394317
Phoenix Lai <plai40@my.bcit.ca>
2022-05-20 00:49:17 -0700

    Phoenix-fixtutorialAccess-mapBackBtn-reloadOnBack

--- public/assets/js/routes/mapSelection.js
+++ public/assets/js/routes/mapSelection.js
@@ -4,20 +4,28 @@ import Cookie from "../helpers/Cookie.js";
 
 const availableMaps = Object.keys(backgroundsCssClassNames);
 
+let selectedMap;
+
 document.querySelector('div.select-done-button').addEventListener('click', event => {
   let selectedMapDiv = document.querySelector('div.card.highlight');
-  let mapName = selectedMapDiv.dataset.mapName;
-  if (!mapName) {
+  selectedMap = selectedMapDiv.dataset.mapName;
+  if (!selectedMap) {
     console.log("No map name is set"); // debug
     return;
   }
 
-  if (!availableMaps.includes(mapName)) {
+  if (!availableMaps.includes(selectedMap)) {
     console.log("Invalid map name"); // debug
     return;
   }
 
-  Cookie.set("selectedMap", mapName);
+  Cookie.set("selectedMap", selectedMap);
 
   location.href = "/";
 });
+
+document.querySelector("div.back-button").addEventListener("click", event => {
+  location.href = "/";
+})
+
+export default selectedMap;
