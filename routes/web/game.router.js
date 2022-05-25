f49fa5eadcc2852d738669f4d063440f7ae9212e
Ilia Abedianamiri <37903573+iliaamiri@users.noreply.github.com>
2022-05-25 10:49:19 -0700

    Update game.router.js

--- routes/web/game.router.js
+++ routes/web/game.router.js
@@ -4,9 +4,10 @@ const gameRouter = express.Router();
 
 // Everything here starts from: /play
 gameRouter.get('/', GameController.showGamePage);
-gameRouter.get('/:gameId', GameController.showOnlinePlay);
 gameRouter.get('/map/selection', GameController.mapSelection);
 gameRouter.get('/customizePigeon', GameController.customizePigeon);
 gameRouter.get('/profile', GameController.profile);
+
+gameRouter.get('/:gameId', GameController.showOnlinePlay);
 
 module.exports = gameRouter;
