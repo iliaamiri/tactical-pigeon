fe7c455dbddd8ba640e7b1837df2b62d79a6a221
Lucia-martin <90855217+Lucia-martin@users.noreply.github.com>
2022-05-25 23:26:11 -0700

    added userHome page with svgs - (accessed with ...localhost:8080/userHome) -no js yet

--- app/http/controllers/homeController.js
+++ app/http/controllers/homeController.js
@@ -2,6 +2,9 @@ const HomeController = {
   async gameHome(req, res) {
     res.render('index');
   },
+  async userHome(req, res) {
+    res.render('userHome');
+  },
 };
 
 module.exports = HomeController;
--- routes/web/home.router.js
+++ routes/web/home.router.js
@@ -4,6 +4,7 @@ const homeRouter = express.Router();
 const HomeController = require('../../app/http/controllers/homeController');
 
 homeRouter.get("/", HomeController.gameHome);
+homeRouter.get("/userHome", HomeController.userHome);
 
 
 module.exports = homeRouter;
