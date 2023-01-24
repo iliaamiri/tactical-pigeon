a8c4504daeca80bd71f6c5e123106e6b21787d98
Ilia Abedian <eilia.abedian@yahoo.com>
2023-01-24 13:45:29 -0800

    dont use multi-threading for now

--- server.js
+++ server.js
@@ -9,20 +9,24 @@ const PORT = configs.PORT;
 const makeApplication = require('./app');
 const server = makeApplication();
 
+server.listen(PORT, () => {
+  console.log(`ProcessId: ${process.pid}. Running on http://0.0.0.0:${PORT}/`);
+});
+
 // TODO: scale express + socket.io using redis
-if (cluster.isMaster && configs.APP_MODE === "production" && false) {
-  console.log(`Running on: http://localhost:${PORT}`);
+// if (cluster.isMaster && configs.APP_MODE === "production" && false) {
+//   console.log(`Running on: http://localhost:${PORT}`);
 
-  for (let cpu of os.cpus()) {
-    cluster.fork();
-  }
+//   for (let cpu of os.cpus()) {
+//     cluster.fork();
+//   }
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`Worker ${worker.process.pid} died (${signal}. Restarting...)`);
-    cluster.fork();
-  });
-} else {
-  server.listen(PORT, () => {
-    console.log(`Worker ${process.pid} is running...`);
-  });
-}
+//   cluster.on('exit', (worker, code, signal) => {
+//     console.log(`Worker ${worker.process.pid} died (${signal}. Restarting...)`);
+//     cluster.fork();
+//   });
+// } else {
+//   server.listen(PORT, () => {
+//     console.log(`Worker ${process.pid} is running...`);
+//   });
+// }
